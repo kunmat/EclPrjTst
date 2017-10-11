f00c1ac6cbc59568dd7b25098d755ef7429f8642
aemk <aemk@AEMK-PC.vfst.int>
2017-10-11 11:14:34 +0200

    GitPlugInTest

---
+++ PrjTst/src/any/ww/TestClassFour.java
@@ -0,0 +1,12 @@
+/*------------------------------------------------------------------------
+Copyright (c) 2017, Verlag für Standesamtswesen
+--------------------------------------------------------------------------*/
+package any.ww;
+
+/**
+ * <tt>TestClassTwo</tt>
+ */
+public class TestClassFour
+{
+
+}
